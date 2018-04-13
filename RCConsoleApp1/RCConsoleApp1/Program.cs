b7ead6027daa94baed8d01095c4a85b101eacf24
Radosław Cygan <radoslaw.cygan@vba.pl>
2018-04-13 17:04:53 +0200

    z

---
+++ RCConsoleApp1/RCConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test 1");
+            Console.ReadKey();
+
+            Console.WriteLine("Test 2");
+            Console.ReadKey();
+
+            Console.ReadKey();
+        }
+    }
+}
